7c2ebdd59ca364abd997d55e78d033941d76160f
Zohre Niayesh <z.ezati@respina.net>
2020-12-22 13:41:09 +0330

    Set up project structure

---
+++ app/src/main/java/com/example/cafebazar/activity/mainscreen/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cafebazar.activity.mainscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cafebazar.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_screen_activity)
+    }
+}
